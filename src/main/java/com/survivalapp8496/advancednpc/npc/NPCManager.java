ed051e5fae19f9545ec47c2d3365b6be80b9585c
survivalapp8496-ctrl <survivalapp8496@gmail.com>
2026-07-05 12:46:33 +0530

    Update NPCManager.java

--- src/main/java/com/survivalapp8496/advancednpc/npc/NPCManager.java
+++ src/main/java/com/survivalapp8496/advancednpc/npc/NPCManager.java
@@ -1,1 +1,55 @@
+package com.survivalapp8496.advancednpc.npc;
 
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class NPCManager {
+
+    private final Map<Integer, NPCData> npcs = new HashMap<>();
+    private int lastId = 0;
+
+    public NPCData createNPC(String name,
+                             EntityType type,
+                             Location location) {
+
+        int id = ++lastId;
+
+        NPCData npc = new NPCData(
+                id,
+                name,
+                type,
+                location
+        );
+
+        npcs.put(id, npc);
+        return npc;
+    }
+
+    public boolean removeNPC(int id) {
+        return npcs.remove(id) != null;
+    }
+
+    public NPCData getNPC(int id) {
+        return npcs.get(id);
+    }
+
+    public Collection<NPCData> getNPCs() {
+        return npcs.values();
+    }
+
+    public int getLastId() {
+        return lastId;
+    }
+
+    public void setLastId(int lastId) {
+        this.lastId = lastId;
+    }
+
+    public Map<Integer, NPCData> getNPCMap() {
+        return npcs;
+    }
+          }
