ff35b2273ca483906cc8cf84cd3d8df3b8edab17
survivalapp8496-ctrl <survivalapp8496@gmail.com>
2026-07-05 12:45:38 +0530

    Create NPCData.java

---
+++ src/main/java/com/survivalapp8496/advancednpc/npc/NPCData.java
@@ -0,0 +1,62 @@
+package com.survivalapp8496.advancednpc.npc;
+
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
+
+public class NPCData {
+
+    private final int id;
+    private String name;
+    private EntityType entityType;
+    private Location location;
+    private NPCPose pose;
+
+    public NPCData(
+            int id,
+            String name,
+            EntityType entityType,
+            Location location
+    ) {
+        this.id = id;
+        this.name = name;
+        this.entityType = entityType;
+        this.location = location;
+        this.pose = NPCPose.NONE;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public EntityType getEntityType() {
+        return entityType;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public NPCPose getPose() {
+        return pose;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEntityType(EntityType entityType) {
+        this.entityType = entityType;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public void setPose(NPCPose pose) {
+        this.pose = pose;
+    }
+  }
