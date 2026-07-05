6768fbdc1017b30fbd21331d7cd3a7adef467ed1
survivalapp8496-ctrl <survivalapp8496@gmail.com>
2026-07-05 12:36:58 +0530

    Update NPCPose.java

--- src/main/java/com/survivalapp8496/advancednpc/npc/NPCPose.java
+++ src/main/java/com/survivalapp8496/advancednpc/npc/NPCPose.java
@@ -1,1 +1,37 @@
 
+package com.survivalapp8496.advancednpc.npc;
+
+public enum NPCPose {
+    NONE,
+
+    // Player poses
+    STANDING,
+    CROUCHING,
+    SWIMMING,
+    SPIN_ATTACK,
+    FALL_FLYING,
+    SLEEPING,
+    DYING,
+
+    // Custom NPC poses
+    SITTING,
+    LYING,
+
+    // Display/animation states
+    WAVING,
+    POINTING,
+    CLAPPING,
+    DANCING,
+
+    // Entity states
+    BABY,
+    ANGRY,
+    CELEBRATING,
+    EATING,
+    ATTACKING,
+    CASTING_SPELL,
+    ROARING,
+    SNIFFING,
+    DIGGING,
+    IDLE
+}
