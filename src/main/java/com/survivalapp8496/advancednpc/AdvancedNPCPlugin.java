0e004da13f6d99239662e33278ed39dfcf952d0e
survivalapp8496-ctrl <survivalapp8496@gmail.com>
2026-07-05 12:52:28 +0530

    Update AdvancedNPCPlugin.java

--- src/main/java/com/survivalapp8496/advancednpc/AdvancedNPCPlugin.java
+++ src/main/java/com/survivalapp8496/advancednpc/AdvancedNPCPlugin.java
@@ -1,27 +1,42 @@
 package com.survivalapp8496.advancednpc;
 
 import com.survivalapp8496.advancednpc.npc.NPCManager;
+import com.survivalapp8496.advancednpc.storage.YamlStorage;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AdvancedNPCPlugin extends JavaPlugin {
 
     private static AdvancedNPCPlugin instance;
+
     private NPCManager npcManager;
+    private YamlStorage storage;
 
     @Override
     public void onEnable() {
         instance = this;
 
+        // Create default files
         saveDefaultConfig();
         saveResource("data.yml", false);
 
+        // Create managers
         npcManager = new NPCManager();
+        storage = new YamlStorage(this);
+
+        // Load NPCs from data.yml
+        storage.loadNPCs(npcManager);
 
         getLogger().info("AdvancedNPC Enabled!");
     }
 
     @Override
     public void onDisable() {
+
+        // Save all NPCs
+        if (storage != null && npcManager != null) {
+            storage.saveNPCs(npcManager);
+        }
+
         getLogger().info("AdvancedNPC Disabled!");
     }
 
@@ -31,5 +46,9 @@ public final class AdvancedNPCPlugin extends JavaPlugin {
 
     public NPCManager getNpcManager() {
         return npcManager;
+    }
+
+    public YamlStorage getStorage() {
+        return storage;
     }
 }
