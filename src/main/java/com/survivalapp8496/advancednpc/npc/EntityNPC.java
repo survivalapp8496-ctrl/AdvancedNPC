1db44c6aef83f28b955d9130946fbed1b6339066
survivalapp8496-ctrl <survivalapp8496@gmail.com>
2026-07-07 10:57:56 +0530

    Create EntityNPC.java

---
+++ src/main/java/com/survivalapp8496/advancednpc/npc/EntityNPC.java
@@ -0,0 +1,127 @@
+package com.survivalapp8496.advancednpc.npc;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.ItemStack;
+
+public class EntityNPC {
+
+    private final NPCData data;
+    private final NPCEntity npcEntity;
+
+    public EntityNPC(NPCData data, NPCEntity npcEntity) {
+        this.data = data;
+        this.npcEntity = npcEntity;
+    }
+
+    public NPCData getData() {
+        return data;
+    }
+
+    public Entity getEntity() {
+        return npcEntity.getEntity();
+    }
+
+    public void teleport(Location location) {
+        npcEntity.teleport(location);
+        data.setLocation(location);
+    }
+
+    public void remove() {
+        npcEntity.remove();
+    }
+
+    public void setGlowing(boolean glowing) {
+
+        if (getEntity() != null) {
+            getEntity().setGlowing(glowing);
+        }
+
+        data.setGlowing(glowing);
+    }
+
+    public void setCustomName(String name) {
+
+        if (getEntity() != null) {
+            getEntity().customName(net.kyori.adventure.text.Component.text(name));
+            getEntity().setCustomNameVisible(true);
+        }
+
+        data.setName(name);
+    }
+
+    public void setHelmet(ItemStack item) {
+
+        if (getEntity() instanceof LivingEntity living) {
+
+            EntityEquipment eq = living.getEquipment();
+
+            if (eq != null) {
+                eq.setHelmet(item);
+            }
+        }
+    }
+
+    public void setChestplate(ItemStack item) {
+
+        if (getEntity() instanceof LivingEntity living) {
+
+            EntityEquipment eq = living.getEquipment();
+
+            if (eq != null) {
+                eq.setChestplate(item);
+            }
+        }
+    }
+
+    public void setLeggings(ItemStack item) {
+
+        if (getEntity() instanceof LivingEntity living) {
+
+            EntityEquipment eq = living.getEquipment();
+
+            if (eq != null) {
+                eq.setLeggings(item);
+            }
+        }
+    }
+
+    public void setBoots(ItemStack item) {
+
+        if (getEntity() instanceof LivingEntity living) {
+
+            EntityEquipment eq = living.getEquipment();
+
+            if (eq != null) {
+                eq.setBoots(item);
+            }
+        }
+    }
+
+    public void setMainHand(ItemStack item) {
+
+        if (getEntity() instanceof LivingEntity living) {
+
+            EntityEquipment eq = living.getEquipment();
+
+            if (eq != null) {
+                eq.setItemInMainHand(item);
+            }
+        }
+    }
+
+    public void setOffHand(ItemStack item) {
+
+        if (getEntity() instanceof LivingEntity living) {
+
+            EntityEquipment eq = living.getEquipment();
+
+            if (eq != null) {
+                eq.setItemInOffHand(item);
+            }
+        }
+    }
+
+}
