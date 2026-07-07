3675f6a5eb71707992664bd9d1cb2c9f6738108e
survivalapp8496-ctrl <survivalapp8496@gmail.com>
2026-07-07 11:07:06 +0530

    Update PacketManager.java

--- src/main/java/com/survivalapp8496/advancednpc/packet/PacketManager.java
+++ src/main/java/com/survivalapp8496/advancednpc/packet/PacketManager.java
@@ -1,1 +1,18 @@
+package com.survivalapp8496.advancednpc.packet;
 
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+
+public class PacketManager {
+
+    private final ProtocolManager protocolManager;
+
+    public PacketManager() {
+        protocolManager = ProtocolLibrary.getProtocolManager();
+    }
+
+    public ProtocolManager getProtocolManager() {
+        return protocolManager;
+    }
+
+}
