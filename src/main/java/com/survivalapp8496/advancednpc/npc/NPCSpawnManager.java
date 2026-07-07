5c957c4bc223d00ef54caf600e0be17bc1ab8664
survivalapp8496-ctrl <survivalapp8496@gmail.com>
2026-07-07 18:24:56 +0530

    Update NPCSpawnManager.java

--- src/main/java/com/survivalapp8496/advancednpc/npc/NPCSpawnManager.java
+++ src/main/java/com/survivalapp8496/advancednpc/npc/NPCSpawnManager.java
@@ -1,11 +1,11 @@
 package com.survivalapp8496.advancednpc.npc;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import com.survivalapp8496.advancednpc.AdvancedNPCPlugin;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class NPCSpawnManager {
 
@@ -22,25 +22,25 @@ public class NPCSpawnManager {
 
         Entity entity = npcEntity.getEntity();
 
-        if (entity != null) {
-
-            entity.setCustomName(data.getName());
-            entity.setCustomNameVisible(true);
-
-            entity.setInvulnerable(true);
-            entity.setSilent(true);
-            entity.setGravity(false);
-            entity.setPersistent(true);
-
-            if (data.isGlowing()) {
-                entity.setGlowing(true);
-            }
-
+        if (entity == null) {
+            return null;
         }
+
+        entity.customName(net.kyori.adventure.text.Component.text(data.getName()));
+        entity.setCustomNameVisible(true);
+        entity.setPersistent(true);
+        entity.setInvulnerable(true);
+        entity.setSilent(true);
+        entity.setGravity(false);
+        entity.setGlowing(data.isGlowing());
 
         EntityNPC entityNPC = new EntityNPC(data, npcEntity);
 
         spawnedNPCs.put(data.getId(), entityNPC);
+
+        AdvancedNPCPlugin.getInstance()
+                .getNpcRegistry()
+                .register(entityNPC);
 
         return entityNPC;
     }
@@ -49,16 +49,20 @@ public class NPCSpawnManager {
 
         EntityNPC npc = spawnedNPCs.remove(id);
 
-        if (npc != null) {
-            npc.remove();
+        if (npc == null) {
+            return;
         }
 
+        npc.remove();
+
+        AdvancedNPCPlugin.getInstance()
+                .getNpcRegistry()
+                .unregister(id);
     }
 
     public void respawn(NPCData data) {
 
         despawn(data.getId());
-
         spawn(data);
 
     }
@@ -67,24 +71,35 @@ public class NPCSpawnManager {
 
         EntityNPC npc = spawnedNPCs.get(id);
 
-        if (npc != null) {
-
-            npc.teleport(location);
-
+        if (npc == null) {
+            return;
         }
 
+        npc.teleport(location);
     }
 
     public EntityNPC getNPC(int id) {
-
         return spawnedNPCs.get(id);
+    }
 
+    public boolean isSpawned(int id) {
+        return spawnedNPCs.containsKey(id);
     }
 
     public Map<Integer, EntityNPC> getSpawnedNPCs() {
-
         return spawnedNPCs;
-
     }
 
+    public void despawnAll() {
+
+        for (EntityNPC npc : spawnedNPCs.values()) {
+            npc.remove();
+        }
+
+        spawnedNPCs.clear();
+
+        AdvancedNPCPlugin.getInstance()
+                .getNpcRegistry()
+                .clear();
+    }
 }
