cd17eab356b0a6327411a9df6b4dffe9256a6c14
survivalapp8496-ctrl <survivalapp8496@gmail.com>
2026-07-07 10:56:10 +0530

    Update NPCData.java

--- src/main/java/com/survivalapp8496/advancednpc/npc/NPCData.java
+++ src/main/java/com/survivalapp8496/advancednpc/npc/NPCData.java
@@ -3,13 +3,27 @@ package com.survivalapp8496.advancednpc.npc;
 import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 
+import java.util.UUID;
+
 public class NPCData {
 
     private final int id;
+
+    private UUID uuid;
+
     private String name;
+
     private EntityType entityType;
+
     private Location location;
+
     private NPCPose pose;
+
+    private String skinName;
+
+    private boolean lookAtPlayer;
+
+    private boolean glowing;
 
     public NPCData(
             int id,
@@ -17,15 +31,28 @@ public class NPCData {
             EntityType entityType,
             Location location
     ) {
+
         this.id = id;
+        this.uuid = UUID.randomUUID();
         this.name = name;
         this.entityType = entityType;
         this.location = location;
         this.pose = NPCPose.STANDING;
+        this.skinName = "";
+        this.lookAtPlayer = true;
+        this.glowing = false;
     }
 
     public int getId() {
         return id;
+    }
+
+    public UUID getUUID() {
+        return uuid;
+    }
+
+    public void setUUID(UUID uuid) {
+        this.uuid = uuid;
     }
 
     public String getName() {
@@ -44,6 +71,18 @@ public class NPCData {
         return pose;
     }
 
+    public String getSkinName() {
+        return skinName;
+    }
+
+    public boolean isLookAtPlayer() {
+        return lookAtPlayer;
+    }
+
+    public boolean isGlowing() {
+        return glowing;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -59,4 +98,17 @@ public class NPCData {
     public void setPose(NPCPose pose) {
         this.pose = pose;
     }
-  }
+
+    public void setSkinName(String skinName) {
+        this.skinName = skinName;
+    }
+
+    public void setLookAtPlayer(boolean lookAtPlayer) {
+        this.lookAtPlayer = lookAtPlayer;
+    }
+
+    public void setGlowing(boolean glowing) {
+        this.glowing = glowing;
+    }
+
+}
