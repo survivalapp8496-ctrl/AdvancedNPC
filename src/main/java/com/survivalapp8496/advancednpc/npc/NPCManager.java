c65b8fcc501bb21a2423d6c7a6a5d3c2190ef419
survivalapp8496-ctrl <survivalapp8496@gmail.com>
2026-07-07 17:24:36 +0530

    Update NPCManager.java

--- src/main/java/com/survivalapp8496/advancednpc/npc/NPCManager.java
+++ src/main/java/com/survivalapp8496/advancednpc/npc/NPCManager.java
@@ -4,12 +4,14 @@ import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class NPCManager {
 
     private final Map<Integer, NPCData> npcs = new HashMap<>();
+
     private int lastId = 0;
 
     public NPCData createNPC(String name,
@@ -26,19 +28,58 @@ public class NPCManager {
         );
 
         npcs.put(id, npc);
+
         return npc;
     }
 
-    public boolean removeNPC(int id) {
-        return npcs.remove(id) != null;
+    public void addNPC(NPCData npc) {
+
+        npcs.put(npc.getId(), npc);
+
+        if (npc.getId() > lastId) {
+            lastId = npc.getId();
+        }
+
+    }
+
+    public boolean containsNPC(int id) {
+        return npcs.containsKey(id);
     }
 
     public NPCData getNPC(int id) {
         return npcs.get(id);
     }
 
+    public boolean removeNPC(int id) {
+
+        if (!npcs.containsKey(id)) {
+            return false;
+        }
+
+        npcs.remove(id);
+
+        return true;
+    }
+
+    public void clear() {
+        npcs.clear();
+        lastId = 0;
+    }
+
     public Collection<NPCData> getNPCs() {
-        return npcs.values();
+        return Collections.unmodifiableCollection(npcs.values());
+    }
+
+    public Map<Integer, NPCData> getNPCMap() {
+        return npcs;
+    }
+
+    public int size() {
+        return npcs.size();
+    }
+
+    public boolean isEmpty() {
+        return npcs.isEmpty();
     }
 
     public int getLastId() {
@@ -49,7 +90,4 @@ public class NPCManager {
         this.lastId = lastId;
     }
 
-    public Map<Integer, NPCData> getNPCMap() {
-        return npcs;
-    }
-          }
+}
