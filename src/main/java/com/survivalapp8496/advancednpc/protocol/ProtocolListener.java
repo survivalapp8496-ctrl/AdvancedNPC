e796968117212c4ad78baef29c81f2898cb44017
survivalapp8496-ctrl <survivalapp8496@gmail.com>
2026-07-09 08:51:01 +0530

    Create ProtocolListener.java

---
+++ src/main/java/com/survivalapp8496/advancednpc/protocol/ProtocolListener.java
@@ -0,0 +1,29 @@
+package com.survivalapp8496.advancednpc.protocol;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+import com.survivalapp8496.advancednpc.AdvancedNPCPlugin;
+
+public class ProtocolListener extends PacketAdapter {
+
+    private final AdvancedNPCPlugin plugin;
+
+    public ProtocolListener(AdvancedNPCPlugin plugin) {
+
+        super(
+                plugin,
+                ListenerPriority.NORMAL,
+                PacketType.Play.Client.USE_ENTITY
+        );
+
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void onPacketReceiving(PacketEvent event) {
+
+        // Left-click handling will be added here
+    }
+}
