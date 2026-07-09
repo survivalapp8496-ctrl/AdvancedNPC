395d068dcdc94cb0998b621961945c00126d3159
survivalapp8496-ctrl <survivalapp8496@gmail.com>
2026-07-09 08:10:59 +0530

    Update NPCListener.java

--- src/main/java/com/survivalapp8496/advancednpc/listener/NPCListener.java
+++ src/main/java/com/survivalapp8496/advancednpc/listener/NPCListener.java
@@ -1,7 +1,11 @@
 package com.survivalapp8496.advancednpc.listener;
 
 import com.survivalapp8496.advancednpc.AdvancedNPCPlugin;
+import com.survivalapp8496.advancednpc.npc.EntityNPC;
+import com.survivalapp8496.advancednpc.npc.NPCData;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
@@ -13,17 +17,67 @@ public class NPCListener implements Listener {
         this.plugin = plugin;
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.NORMAL)
     public void onRightClick(PlayerInteractEntityEvent event) {
 
-        plugin.getLogger().info(
-                event.getPlayer().getName()
-                        + " clicked "
-                        + event.getRightClicked().getType()
-        );
+        Player player = event.getPlayer();
 
-        // TODO: Get NPC from clicked entity
-        // TODO: Permission check
-        // TODO: Execute NPC commands
+        EntityNPC entityNPC =
+                plugin.getNpcSpawnManager().getNPC(event.getRightClicked());
+
+        if (entityNPC == null) {
+            return;
+        }
+
+        event.setCancelled(true);
+
+        NPCData npc = entityNPC.getData();
+
+        // Permission Check
+        if (npc.isPermissionEnabled()) {
+
+            if (!player.hasPermission("advancednpc.bypass.permission")
+                    && !player.hasPermission(npc.getPermission())) {
+
+                player.sendMessage(npc.getPermissionMessage());
+                return;
+            }
+        }
+
+        executeCommands(player, npc);
+    }
+
+    private void executeCommands(Player player, NPCData npc) {
+
+        for (String command : npc.getCommands()) {
+
+            command = command.replace("%player%", player.getName());
+
+            if (command.startsWith("console:")) {
+
+                String cmd = command.substring(8);
+
+                plugin.getServer().dispatchCommand(
+                        plugin.getServer().getConsoleSender(),
+                        cmd
+                );
+
+                continue;
+            }
+
+            if (command.startsWith("player:")) {
+
+                String cmd = command.substring(7);
+
+                player.performCommand(cmd);
+
+                continue;
+            }
+
+            plugin.getServer().dispatchCommand(
+                    plugin.getServer().getConsoleSender(),
+                    command
+            );
+        }
     }
 }
